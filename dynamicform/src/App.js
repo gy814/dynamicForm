802834f4908e82e433cb72209d935536a4c2fc50
unknown <gy92814@gmail.com>
2019-04-19 16:09:33 +1000

    create form component

--- dynamicform/src/App.js
+++ dynamicform/src/App.js
@@ -2,8 +2,132 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Form extends Component{
+  render(){
+    return (
+      <div>
+        <form></form>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
+    const bmiReferenceProps = {
+      id: 'bmi',
+      observationName: 'BMI - Body mass index',
+      dataElements: [
+        {
+          id: 'name',
+          displayName: 'Name',
+          type: 'textInput',
+          display: true,
+          isRequired: true,
+        },
+        {
+          id: 'gender',
+          displayName: 'Gender',
+          type: 'select',
+          options: [
+            {
+              id: 1,
+              name: 'Male',
+              isDefault: true,
+              sortOrder: 1,
+            },
+            {
+              id: 2,
+              name: 'Female',
+              isDefault: false,
+              sortOrder: 2,
+            },
+          ],
+          display: true,
+          isRequired: false,
+        },
+        {
+          id: 'weight',
+          displayName: 'Weight',
+          unitOfMeasure: 'kg',
+          type: 'numberInput',
+          bounds: {
+            upperLimit: 1000,
+          },
+          display: true,
+          isRequired: true,
+        },
+        {
+          id: 'height',
+          displayName: 'Height',
+          unitOfMeasure: 'cm',
+          type: 'numberInput',
+          bounds: {
+            upperLimit: 300,
+          },
+          display: true,
+          isRequired: true,
+        },
+        {
+          id: 'bmi',
+          displayName: 'BMI',
+          unitOfMeasure: 'kg/m2',
+          type: 'numberInput',
+          bounds: {
+            upperLimit: 100,
+          },
+          display: false,
+          isRequired: false,
+        },
+      ],
+    };
+    
+    const headCircumferenceReferenceProps = {
+      id: 'head-circumference',
+      observationName: 'Head Circumference',
+      dataElements: [
+        {
+          id: 'name',
+          displayName: 'Name',
+          type: 'textInput',
+          display: true,
+          isRequired: true,
+        },
+        {
+          id: 'gender',
+          displayName: 'Gender',
+          type: 'select',
+          options: [
+            {
+              id: 1,
+              name: 'Male',
+              isDefault: true,
+              sortOrder: 1,
+            },
+            {
+              id: 2,
+              name: 'Female',
+              isDefault: false,
+              sortOrder: 2,
+            },
+          ],
+          display: true,
+          isRequired: false,
+        },
+        {
+          id: 'head-circumference',
+          displayName: 'Head Circumference',
+          unitOfMeasure: 'cm',
+          type: 'numberInput',
+          bounds: {
+            upperLimit: 1000,
+          },
+          display: true,
+          isRequired: true,
+        },
+      ],
+    };
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -20,6 +144,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Form formData={headCircumferenceReferenceProps}/>
       </div>
     );
   }
